a02ba903d8698054ba61a9d9143398e59e1c7424
heeryung <heeryung.c@gmail.com>
2016-09-22 16:33:03 -0400

    -

--- mit_lti_flask_sample.py
+++ mit_lti_flask_sample.py
@@ -51,7 +51,7 @@ def redirect_to(lti=lti) :
 @app.route('/index', methods=['GET', 'POST'])
 # @app.route('/lti/', methods=['GET', 'POST'])
 # request='any',
-@lti(error=error, app=app)
+# @lti(error=error, app=app)
 def index(lti=lti):
     """ initial access page to the lti provider.  This page provides
     authorization for the user.
